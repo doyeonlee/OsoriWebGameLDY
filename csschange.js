107bd07249e6fbd16e7e2f75f3e6916b9094ecbd
2017030091 <2017030091@hanyang.ac.kr>
2018-02-10 19:27:29 +0900

    viewRate

---
+++ csschange.js
@@ -0,0 +1,9 @@
+function change() {
+var main_image = document.getElementById("main_background");
+var main_start = document.getElementById("main_start");
+var main_set = document.getElementById("main_setting");
+main_image.style.backgroundImage="url('./image/main_window2.JPG')";
+main_start.style.left="30%";
+main_set.style.top="80%";
+main_set.style.left="80%";
+}
